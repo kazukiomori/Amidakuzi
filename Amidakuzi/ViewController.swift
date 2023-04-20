19181af6dcef1a9e5b4e32aee6ce3f13d52d6047
kazukiomori <kazuki1110170501@gmail.com>
2023-04-20 23:42:21 +0900

    Initial Commit

---
+++ Amidakuzi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amidakuzi
+//
+//  Created by Kazuki Omori on 2023/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
