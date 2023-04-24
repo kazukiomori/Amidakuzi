cad3b971c2bd118829e78bca05d3f3832e34db5a
kazukiomori <kazuki1110170501@gmail.com>
2023-04-24 22:54:35 +0900

    ColorのExtensionを生成する

---
+++ Amidakuzi/Extension/Color+Extension.swift
@@ -0,0 +1,47 @@
+//
+//  Color+Extension.swift
+//  Amidakuzi
+//
+//  Created by Kazuki Omori on 2023/04/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static func red() -> UIColor {
+        return UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1.0)
+    }
+    static func blue() -> UIColor {
+        return UIColor(red: 0/255, green: 50/255, blue: 203/255, alpha: 1.0)
+    }
+    static func lightBlue() -> UIColor {
+        return UIColor(red: 0/255, green: 255/255, blue: 255/255, alpha: 1.0)
+    }
+    static func purple() -> UIColor {
+        return UIColor(red: 127/255, green: 0/255, blue: 255/255, alpha: 1.0)
+    }
+    static func pink() -> UIColor {
+        return UIColor(red: 255/255, green: 0/255, blue: 255/255, alpha: 1.0)
+    }
+    static func yellow() -> UIColor {
+        return UIColor(red: 255/255, green: 255/255, blue: 50/255, alpha: 1.0)
+    }
+    static func orange() -> UIColor {
+        return UIColor(red: 255/255, green: 127/255, blue: 0/255, alpha: 1.0)
+    }
+    static func yellowGreen() -> UIColor {
+        return UIColor(red: 127/255, green: 255/255, blue: 0/255, alpha: 1.0)
+    }
+    static func green() -> UIColor {
+        return UIColor(red: 0/255, green: 152/255, blue: 0/255, alpha: 1.0)
+    }
+    static func gray() -> UIColor {
+        return UIColor(red: 127/255, green: 127/255, blue: 127/255, alpha: 1.0)
+    }
+    static func black() -> UIColor {
+        return UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+    }
+    static func white() -> UIColor {
+        return UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0)
+    }
+}
