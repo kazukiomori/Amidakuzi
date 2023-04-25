a7674fb28ea2e66ada6fc8794b2089942e9cab02
kazukiomori <kazuki1110170501@gmail.com>
2023-04-26 01:40:04 +0900

    cell内のtextFieldの文字を配列に格納する関数を作成

--- Amidakuzi/ViewController/MemberViewController.swift
+++ Amidakuzi/ViewController/MemberViewController.swift
@@ -22,6 +22,11 @@ class MemberViewController: UIViewController, UITableViewDelegate, UITableViewDa
         navigationItemSet()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startItems = []
+    }
+    
     // MARK: Method
     func navigationItemSet() {
         let addBarButtonItem = UIBarButtonItem(title: "作成", style: .done, target: self, action: #selector(showNextView))
@@ -33,9 +38,22 @@ class MemberViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         guard let nextViewController = storyBoard.instantiateViewController(withIdentifier: "AmidakuziViewController") as? AmidakuziViewController else { return }
+        saveTextFieldValues()
         nextViewController.startItems = self.startItems
         nextViewController.goalItems = self.goalItems
         self.navigationController?.show(nextViewController, sender: nil)
+    }
+    
+    func saveTextFieldValues() {
+        // TableViewの全てのセルをループしてTextFieldの値を配列に格納する
+        for section in 0..<tableView.numberOfSections {
+            for row in 0..<tableView.numberOfRows(inSection: section) {
+                if let cell = tableView.cellForRow(at: IndexPath(row: row, section: section)) as? MemberTableViewCell {
+                    // TextFieldの値を配列に追加する
+                    startItems.append(cell.memberTextField.text ?? "")
+                }
+            }
+        }
     }
     
     // MARK: TableView
@@ -45,6 +63,7 @@ class MemberViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MemberTableViewCell", for: indexPath) as? MemberTableViewCell else {return UITableViewCell()}
+        cell.memberTextField.text = "メンバー\(indexPath.row)"
         cell.colorView.layer.cornerRadius = cell.colorView.frame.height / 2
         cell.colorView.backgroundColor = colors[indexPath.row]
         return cell
